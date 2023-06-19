8b32fb3c264f95780dabcc3a89a862252a34f49d
jhagg26 <jhagg26@gmail.com>
2023-06-19 18:40:55 -0400

    Add elements to HTML

--- src/index.js
+++ src/index.js
@@ -10,15 +10,13 @@ function createInitialWebpage() {
     var content = document.createElement("main");
     content.setAttribute("id", "content");
 
-    var footer = document.createElement("footer");
-    footer.textContent = "Footer";
-
-
     var navbar = document.createElement("navbar");
-    navbar.textContent = "Navbar";
 
     var todoSection = document.createElement("article");
-    todoSection.textContent = "Article";
+    todoSection.textContent = "To Do";
+
+    var footer = document.createElement("footer");
+    footer.textContent = "Footer";
 
 
     content.appendChild(navbar);
@@ -31,7 +29,48 @@ function createInitialWebpage() {
     return container;
 }
 
+function populateNavbar() {
+    var navbar = document.querySelector("navbar");
+
+    var tasksSection = document.createElement('div');
+    tasksSection.setAttribute("id", "tasksSection");
+
+    var tasks = document.createElement('div');
+    tasks.setAttribute("id", "tasks");
+    tasks.textContent = "Tasks";
+
+    var today = document.createElement('div');
+    today.setAttribute("id", "today");
+    today.textContent = "Today";
+
+    var month = document.createElement('div');
+    month.setAttribute("id", "month");
+    month.textContent = "Month";
+
+    tasksSection.appendChild(tasks);
+    tasksSection.appendChild(today);
+    tasksSection.appendChild(month);
+
+    var projectsSection = document.createElement('div');
+    projectsSection.setAttribute("id", "projectsSection");
+
+    var projectsText = document.createElement("h1");
+    projectsText.textContent = "Projects";
+
+    var addProjects = document.createElement('div');
+    addProjects.textContent = "+ Add Project";
+    
+    projectsSection.appendChild(projectsText);
+    projectsSection.appendChild(addProjects);
+
+    navbar.appendChild(tasksSection);
+    navbar.appendChild(projectsSection);
+}
+
 
 
 const container = createInitialWebpage();
 document.body.appendChild(container);
+
+
+populateNavbar();
